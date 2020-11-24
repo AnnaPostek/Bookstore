2d30d603cb1d0a2c0fdc99e601635dc7d7e31bb7
Ania Postek <postek-anna@wp.pl>
2020-11-24 13:02:45 +0100

    [update] some correction with adding books

--- src/main/java/pl/postek/final_shop/model/entity/Category.java
+++ src/main/java/pl/postek/final_shop/model/entity/Category.java
@@ -16,7 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 public class Category {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String categoryName;
     @OneToMany
