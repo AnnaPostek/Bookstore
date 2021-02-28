2c0cbecb223c15738eebf7e34f63cd8c8780dda2
Ania Postek <postek-anna@wp.pl>
2021-02-28 16:32:56 +0100

    [update] homePage is updated

--- src/main/java/pl/postek/final_shop/model/dto/BookDto.java
+++ src/main/java/pl/postek/final_shop/model/dto/BookDto.java
@@ -5,9 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
